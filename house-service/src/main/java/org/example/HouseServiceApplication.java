431a434853d34193ac92184834cc66bc8774c8fe
我是一只鸵鸟 <wsyztn@163.com>
2026-03-29 19:13:08 +0800

    house-service-Init

---
+++ house-service/src/main/java/org/example/HouseServiceApplication.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/**
+ * @author W.W.
+ * @since 2026-03-29
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HouseServiceApplication.class, args);
+    }
+}
